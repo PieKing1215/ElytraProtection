10d47ffd70bac4ef40849b7f95f75f5784948d76
David M <PieKing1215@users.noreply.github.com>
2021-06-10 17:26:24 -0400

    Update to 1.17

--- src/main/java/me/pieking1215/elytraprotection/mixin/MixinProtectionEnchantment.java
+++ src/main/java/me/pieking1215/elytraprotection/mixin/MixinProtectionEnchantment.java
@@ -1,26 +1,18 @@
 package me.pieking1215.elytraprotection.mixin;
 
-import net.minecraft.enchantment.Enchantment;
 import net.minecraft.enchantment.EnchantmentTarget;
 import net.minecraft.enchantment.ProtectionEnchantment;
-import net.minecraft.entity.EquipmentSlot;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.ModifyArg;
 
 @Mixin(ProtectionEnchantment.class)
 public class MixinProtectionEnchantment {
 
-    @Inject(at = @At("TAIL"), method = "<init>")
-    private void init(Enchantment.Rarity weight, ProtectionEnchantment.Type type, EquipmentSlot[] slotTypes, CallbackInfo callback){
-        ProtectionEnchantment self = (ProtectionEnchantment)(Object)this;
-
-        // change protection enchantments to be available on all wearables (includes blast/fire/proj prot)
-        // TODO: come up with a way to make this only apply to elytra (mixin a new EnchantmentTarget?)
-        if(self.type == EnchantmentTarget.ARMOR) {
-            ((EnchantmentAccessor)self).setType(EnchantmentTarget.WEARABLE);
-        }
+    @ModifyArg(method = "<init>", at = @At(value = "INVOKE", target = "Lnet/minecraft/enchantment/Enchantment;<init>(Lnet/minecraft/enchantment/Enchantment$Rarity;Lnet/minecraft/enchantment/EnchantmentTarget;[Lnet/minecraft/entity/EquipmentSlot;)V"))
+    private static EnchantmentTarget inj(EnchantmentTarget orig){
+        if(orig == EnchantmentTarget.ARMOR) return EnchantmentTarget.WEARABLE;
+        return orig;
     }
 
 }
